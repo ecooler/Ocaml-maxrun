ab37e6ef9984ce4dfc8817119163749d0bb3ff94
powcoder代写 <powcoder@163.com>
2022-07-16 16:25:32 +0800

    程序代写 加微信 powcoder Programming Help Add Wechat powcoder

---
+++ submission.ml
@@ -0,0 +1,14 @@
+
+(** Problem 4 **)
+
+type run_output = 
+{  length : int;
+   entries : int list;
+}
+
+let maxrun (l: int list) : run_output =  
+  (** YOUR CODE HERE **)
+  {length = 1; entries =[]}
+
+
+
